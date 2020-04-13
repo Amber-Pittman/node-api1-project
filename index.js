c364092b356e21c2b9784ff308b24f0122f5f136
Amber-Pittman <amber@amberpittman.com>
2020-04-13 11:46:31 -0400

    Working on POST request

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const db = require("./database.js")
 
 const server = express();
+server.use(express.json());
 
-server.get("/", (req, res) => {
-    res.send("Hello World from Express!");
+server.get("/api", (req, res) => {
+    res.send("Hello World from the API!");
 });
+
+server.post("/api/users", (req, res) => {
+    if (!req.body.name || !req.body.bio) {
+        return res.status(400).json({
+            message: "Need a name AND a bio",
+        })
+    }
+
+    const newUser = db.createUser({
+        name: req.body.name,
+        bio: req.body.bio
+    })
+    res.status(201).json(newUser)
+})
 
 server.listen(5000, () => 
     console.log(`Success! API running on http://localhost:5000`)

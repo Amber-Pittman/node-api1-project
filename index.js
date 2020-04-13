69308fd45e501cf817c27523f8d70b6c5c83ec67
Amber-Pittman <amber@amberpittman.com>
2020-04-13 13:05:52 -0400

    Working on requests

--- index.js
+++ index.js
@@ -8,23 +8,38 @@ server.get("/", (req, res) => {
     res.send("Hello World from the API!");
 });
 
-server.get("/api/users", (req, res) => {
-    const users = db.getUsers(); // get list of users
-    res.json(users);
-})
-
 server.post("/api/users", (req, res) => {
     if (!req.body.name || !req.body.bio) {
         return res.status(400).json({
-            message: "Need a name AND a bio",
-        })
+            errorMessage: "Please provide name AND bio for the user.",
+        });
     }
-
+     
     const newUser = db.createUser({
         name: req.body.name,
         bio: req.body.bio
     })
-    res.status(201).json(newUser)
+
+    if (newUser) {
+        res.status(201).json(newUser);
+    } else {
+        res.status(500).json({
+            errorMessage: "There was an error while saving the user to the database"
+        })
+    }    
+})
+
+server.get("/api/users", (req, res) => {
+    const users = db.getUsers(); // get list of users
+
+    if (users) {
+        res.json(users);
+    } else {
+        res.status(500).json({
+            errorMessage: "The users information could not be retrieved."
+        })
+    }
+    
 })
 
 server.get("/api/users/:id", (req, res) => {
@@ -34,8 +49,33 @@ server.get("/api/users/:id", (req, res) => {
     if (user) {
         res.json(user)
     } else {
+        res.status(404).json({
+            message: "The user with the specified ID does not exist.",
+        })
+    }
+
+    if (!user) {
         res.status(500).json({
-            message: "User not found",
+            errorMessage: "The user information could not be retrieved."
+        })
+    }
+})
+
+server.delete("/api/users/:id", (req, res) => {
+    const user = db.getUserById(req.params.id)
+
+    if (user) {
+        db.deleteUser(user.id)
+        res.status(204).end()
+    } else {
+        res.status(404).json({
+            message: "The user with the specified ID does not exist."
+        })
+    }
+
+    if (!user) {
+        res.status(500).json({
+            errorMessage: "The user could not be removed"
         })
     }
 })
@@ -51,23 +91,20 @@ server.put("/api/users/:id", (req, res) => {
         res.json(updateUser)
     } else {
         res.status(404).json({
-            message: "User not found"
+            message: "The user with the specified ID does not exist."
         })
-    }
+    } 
+    // else {
+    //     (!updateUser) {
+    //         res.status(500).json({
+    //             errorMessage: "The user information could not be modified.",
+    //         })
+    //     }
+    // }
+    res.status(200).json(updateUser);
 })
 
-server.delete("/api/users/:id", (req, res) => {
-    const user = db.getUserById(req.params.id)
 
-    if (user) {
-        db.deleteUser(user.id)
-        res.status(204).end()
-    } else {
-        res.status(404).json({
-            message: "User not found - can't delete"
-        })
-    }
-})
 
 server.listen(5000, () => 
     console.log(`Success! API running on http://localhost:5000`)

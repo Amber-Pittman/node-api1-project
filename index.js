7b579ee617ec6c4eaebf6b49d9b98ced4df6697f
Amber-Pittman <amber@amberpittman.com>
2020-04-13 12:19:50 -0400

    Working on requests

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const db = require("./database.js")
 const server = express();
 server.use(express.json());
 
-server.get("/api", (req, res) => {
+server.get("/", (req, res) => {
     res.send("Hello World from the API!");
 });
+
+server.get("/api/users", (req, res) => {
+    const users = db.getUsers(); // get list of users
+    res.json(users);
+})
 
 server.post("/api/users", (req, res) => {
     if (!req.body.name || !req.body.bio) {
@@ -20,6 +25,48 @@ server.post("/api/users", (req, res) => {
         bio: req.body.bio
     })
     res.status(201).json(newUser)
+})
+
+server.get("/api/users/:id", (req, res) => {
+    const userId = req.params.id
+    const user = db.getUserById(userId)
+
+    if (user) {
+        res.json(user)
+    } else {
+        res.status(500).json({
+            message: "User not found",
+        })
+    }
+})
+
+server.put("/api/users/:id", (req, res) => {
+    const user = db.getUserById(req.params.id)
+
+    if (user) {
+        const updateUser = db.updateUser(user.id, {
+            name: req.body.name || user.name,
+            bio: req.body.bio || user.bio,
+        })
+        res.json(updateUser)
+    } else {
+        res.status(404).json({
+            message: "User not found"
+        })
+    }
+})
+
+server.delete("/api/users/:id", (req, res) => {
+    const user = db.getUserById(req.params.id)
+
+    if (user) {
+        db.deleteUser(user.id)
+        res.status(204).end()
+    } else {
+        res.status(404).json({
+            message: "User not found - can't delete"
+        })
+    }
 })
 
 server.listen(5000, () => 
